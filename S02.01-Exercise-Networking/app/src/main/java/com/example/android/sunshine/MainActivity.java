cbc843840169f4f29d3191ee2abb0e6a5800c810
EricAndreR <ericandre89@gmail.com>
2017-11-14 20:58:02 +0000

    Created networkRequest and set up doInBackground.

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -15,9 +15,15 @@
  */
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,7 +48,22 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
 
-    // TODO (5) Create a class that extends AsyncTask to perform network requests
-    // TODO (6) Override the doInBackground method to perform your network requests
+    // COMPLETED (5) Create a class that extends AsyncTask to perform network requests
+    public class networkRequest extends AsyncTask<URL, void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchNetwork = urls[0];
+            String networkResult =  null;
+            try {
+                networkResult = NetworkUtils.getResponseFromHttpUrl(searchNetwork);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return networkResult;
+        }
+
+    }
+    // COMPLETED (6) Override the doInBackground method to perform your network requests
     // TODO (7) Override the onPostExecute method to display the results of the network request
 }
